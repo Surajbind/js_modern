9cfdf9fd7d65d78344d424453cb7b8c491a283ad
Surajbind <52559224+Surajbind@users.noreply.github.com>
2025-04-09 10:00:00 +0000

    added primitive and non-primitive

--- Basics/2datatype.js
+++ Basics/2datatype.js
@@ -51,10 +51,40 @@ const obj = {
 
 const printFunc = function(){
     for(let i=0;i<=arr.length;i++){
-        console.log(" "+arr[i]);
+        // console.log(" "+arr[i]);
     }
 
-    console.table([obj.name,obj.id,obj.isElegible]);
+    // console.table([obj.name,obj.id,obj.isElegible]);
 }
 
 printFunc();
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//(Stack) --Primitive data Type get stored inside Stack 
+//--> Stack create and give a copy of og memory 
+
+let name = "James";//first value was Suraj
+let copyName = name;
+
+copyName = "Nex";
+// console.table([name,copyName]);
+//copyName doesnot overwrite the primitive data type
+
+
+
+//(Heap)  --Non primitive data Type get stored inside Heap
+//--> Heap gives a refrence of original data 
+
+const letObj = {
+    id:123,
+    name:"Suraj",
+    passWord:"suRaj"
+}
+
+let newObj = letObj;
+newObj.name = "James"; //it will update the og data by overwritting the data  as it uses refrence of og data 
+console.log(letObj.name); 
