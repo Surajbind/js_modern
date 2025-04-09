2b584ff5acacb044239a51c097279667fd227360
Surajbind <52559224+Surajbind@users.noreply.github.com>
2025-04-09 09:45:45 +0000

    Data types added

--- Basics/2datatype.js
+++ Basics/2datatype.js
@@ -8,8 +8,8 @@ let accountCity = "Chile";
 
 let accountState;
 
-console.log(accoundId);
-console.table([accoundId,accountPassword,accountCity,accountState]);
+// console.log(accoundId);
+// console.table([accoundId,accountPassword,accountCity,accountState]);
 
 /* Never Use Var
     As it does not make use of scope 
@@ -30,8 +30,31 @@ console.table([accoundId,accountPassword,accountCity,accountState]);
 
 //object 
 
-console.log(typeof null); //object
-console.log(typeof Object); //function
-console.log(typeof undefined);//undefined
+// console.log(typeof null); //object
+// console.log(typeof Object); //function
+// console.log(typeof undefined);//undefined
 
 
+// Primitive data types
+//String, Boolean , Null, Undefined , Symbol, BigInt, Number
+
+
+// Reference (Non-Primitive) 
+//Array, Object, Functions
+const arr = ['ArrayD1','ArrayD2','ArrayD3','ArrayD4']; //non primitive refrence type data 
+
+const obj = {
+    name:"Suraj",
+    id:12,
+    isElegible:true
+}
+
+const printFunc = function(){
+    for(let i=0;i<=arr.length;i++){
+        console.log(" "+arr[i]);
+    }
+
+    console.table([obj.name,obj.id,obj.isElegible]);
+}
+
+printFunc();
