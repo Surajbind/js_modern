f4fc1faa7592ce9e29a312e06696d2ae77bef7f7
Surajbind <52559224+Surajbind@users.noreply.github.com>
2025-04-09 08:21:49 +0000

    Variable and there scope

---
+++ Basics/2variable.js
@@ -0,0 +1,10 @@
+// this is a comment
+
+const accoundId = 123345;
+var accountPassword = "ThisisP@WD";
+let accountCity = "Chile";
+
+let accountState;
+
+console.log(accoundId);
+console.table([accoundId,accountPassword,accountCity,accountState]);
