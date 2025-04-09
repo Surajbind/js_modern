7bac602545a86115532cd5b28ce42af9653889e2
Surajbind <52559224+Surajbind@users.noreply.github.com>
2025-04-09 08:38:11 +0000

    Added Basic Data Types

---
+++ Basics/2datatype.js
@@ -0,0 +1,37 @@
+// this is a comment
+
+"use strict";// treats all js code as newer version code
+
+const accoundId = 123345;
+var accountPassword = "ThisisP@WD";
+let accountCity = "Chile";
+
+let accountState;
+
+console.log(accoundId);
+console.table([accoundId,accountPassword,accountCity,accountState]);
+
+/* Never Use Var
+    As it does not make use of scope 
+    Use Let as it always uses scope
+*/
+
+//type of data type
+
+//number => 
+//big Inte =>
+//String => "text"
+//boolean => true/false
+//null => standalone value
+
+//undefined => undefined
+
+//symbol => unique
+
+//object 
+
+console.log(typeof null); //object
+console.log(typeof Object); //function
+console.log(typeof undefined);//undefined
+
+
