94c21780101c371c107597504b93432caf04ada5
Surajbind <52559224+Surajbind@users.noreply.github.com>
2025-04-09 09:22:52 +0000

    Basic Comparision and Conversion

---
+++ Basics/3conversionandoperation.js
@@ -0,0 +1,58 @@
+let num = 33;
+let num2 = "33";
+let num3 = "33abc"
+
+let isNumber = Number(num2);
+let isNumber2 = Number(num3);
+
+// console.log(isNumber2);
+
+
+// console.table([typeof num,typeof num2,typeof isNumber, typeof isNumber2]);
+
+/*
+
+Hence Basic Data type
+
+"33"=> 33 
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+
+/* 
+Type of Boolean
+
+1=> true;
+
+0=>false;
+
+""=> false;
+
+"Suraj"=> true
+
+*/
+
+
+
+
+// ##################{Operation}#####################
+let value = 3;
+let negVal = -value;
+
+let vs = 2+negVal;
+
+// console.log(vs);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// conversion into string
+// console.log(1+1);
+// console.log("1"+1+2); //String in the Start will convert everything into String
+// console.log(1+1+"2"); //String in the end will add in the starting variables
+
